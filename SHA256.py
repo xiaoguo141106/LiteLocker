0dd0758f0b1b81d6ad618da85e2fc0ec338873ee
xiaoguo141106 <2160717774@qq.com>
2026-02-15 23:37:35 +0800

    哈希转换

--- SHA256.py
+++ SHA256.py
@@ -2,7 +2,7 @@ import streamlit as st
 import hashlib
 
 # 假设你的原始密码是 asdasd123321xg
-raw_password = "asdasd123321xg"
+raw_password = input("请输入需要转换的值")
 
 # 使用 SHA-256 算法加密
 password_hash = hashlib.sha256(raw_password.encode()).hexdigest()
